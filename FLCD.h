dee31bfa909fad8516636e7630be232e78a64797
Paolo D'Apice <dapicester@gmail.com>
2012-12-11 19:51:30 +0800

    Added GPLv2

--- FLCD.h
+++ FLCD.h
@@ -1,20 +1,23 @@
 /*
- * FlamingoEda LCD display library.
- *
- * This library provides a simple API for the LCD
- * provided by FlamingoEda.
- * The LCD display uses serial communication and responds
- * to commands written in the format "$COMMAND arguments\r\n".
- * The provided API composed of the follwing commands:
- * - $CLEAR      clear the screen
- * - $GO x y     move the cursor to the x-th line y-th column
- * - $PRINT text print the text
- * - $CURSOR a b enable/disable the cursor display and blinking
- *
- * Library written by Paolo D'Apice <paolo.dapice@gmail.com>
- *
- * This code is in the public domain.
- */
+  FLCD.h - FlamingoEda LCD display library.
+
+  Copyright (c) 2012 Paolo D'Apice <paolo.dapice@gmail.com>.
+  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 
 #ifndef FLCD_H
 #define FLCD_H
